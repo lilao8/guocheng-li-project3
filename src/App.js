24cd7ea619a28398e5208f51a8b885169b94da88
lilao8 <liamgmai1@gmail.com>
2021-07-26 23:59:08 -0400

    css updated, new useEffect for profit value

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
   const [price, setPrice] = useState("");
   const [share, setShare] = useState("");
   const [stockList, setStockList] = useState([]);
+  const [newList, setNewList] = useState([]);
 
   useEffect(() => {
     const dbRef = firebase.database().ref();
-
     dbRef.on("value", (response) => {
       const myData = response.val();
-      console.log(myData);
+
       const newArray = [];
-      const key = "JCWSLD5KCL4HGL0Q";
+
       for (const stocks in myData) {
         const stock = {
           key: stocks,
@@ -24,30 +24,9 @@ function App() {
           price: myData[stocks].price,
           share: myData[stocks].share,
         };
-        console.log(stock);
         newArray.push(stock);
-        axios({
-          url: "https://www.alphavantage.co/query",
-          method: "GET",
-          params: {
-            function: "TIME_SERIES_DAILY",
-            symbol: stock.symbol,
-            apikey: key,
-          },
-        }).then((res) => {
-          console.log(res.data);
-          const inputPrice = stock.price;
-          const todayPrice =
-            res.data["Time Series (Daily)"]["2021-07-23"]["4. close"];
-          const profitPerShare = todayPrice - inputPrice;
-          const profitPerStock = profitPerShare * stock.share
-          console.log(profitPerStock);
-        }).catch((error)=>{
-          alert(error,'Please enter correct information or wait for another minute.')
-        })
       }
       setStockList(newArray);
-      console.log(stockList);
     });
   }, []);
 
@@ -71,12 +50,48 @@ function App() {
     dbRef.child(item).remove();
   };
 
+  const handleCal = (event) => {
+    event.preventDefault();
+    const newArray = [];
+    const key = "JCWSLD5KCL4HGL0Q";
+    let totalProfit = 0;
+
+    stockList.map((stockObj) => {
+      axios({
+        url: "https://www.alphavantage.co/query",
+        method: "GET",
+        params: {
+          function: "TIME_SERIES_DAILY",
+          symbol: stockObj.symbol,
+          apikey: key,
+        },
+      })
+        .then((res) => {
+          const inputPrice = stockObj.price;
+          const todayPrice =
+            res.data["Time Series (Daily)"]["2021-07-23"]["4. close"];
+          const profitPerStock =
+            (Math.round(todayPrice - inputPrice) * stockObj.share * 100) / 100;
+          totalProfit += profitPerStock;
+          const profit = { profit: profitPerStock, total: totalProfit };
+          const stock1 = { ...stockObj, ...profit };
+          newArray.push(stock1);
+          console.log(stock1);
+        })
+        .catch((error) => {
+          console.log(
+            error,
+            "Please enter correct information or wait for another minute."
+          );
+        });
+      setNewList(newArray);
+    });
+  };
+
   return (
     <div className="App">
       <header>
-        <h1>Stock Watchlist</h1>
-      </header>
-      <main>
+        <h1>Stock Watchlist📈📈📈</h1>
         <form action="submit" onSubmit={handleSubmit}>
           <input
             type="text"
@@ -100,38 +115,55 @@ function App() {
             }}
             value={share}
           />
-          <button type="submit">+</button>
+          <button>+</button>
         </form>
+      </header>
 
-        <table>
-          <thead>
-            <tr>
-              <th>Symbol</th>
-              <th>Price</th>
-              <th>Share</th>
-            </tr>
-          </thead>
-          {stockList.map((stockObject) => {
-            return (
-              <tbody key={stockObject.key}>
-                <tr>
-                  <td>{stockObject.symbol}</td>
-                  <td>{stockObject.price}</td>
-                  <td>{stockObject.share}</td>
-                  <td>
-                    <button
-                      onClick={() => {
-                        handleDelete(stockObject.key);
-                      }}
-                    >
-                      -
-                    </button>
-                  </td>
-                </tr>
-              </tbody>
-            );
-          })}
-        </table>
+      <main>
+        <div className="tables wrapper">
+          <table>
+            <thead>
+              <tr>
+                <th>Symbol</th>
+                <th>Price</th>
+                <th>Share</th>
+              </tr>
+            </thead>
+            <tbody>
+              {stockList.map((stockObject) => {
+                return (
+                  <tr key={stockObject.key}>
+                    <td>{stockObject.symbol}</td>
+                    <td>{stockObject.price}</td>
+                    <td>{stockObject.share}</td>
+                    <td>
+                      <button
+                        onClick={() => {
+                          handleDelete(stockObject.key);
+                        }}
+                      >
+                        -
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <table>
+            <tbody>
+              <th>Profit</th>
+              {newList.map((profitObject) => {
+                return (
+                  <tr key={profitObject.key}>
+                    <td>{profitObject.profit}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <button className = "submit"onClick={handleCal}>Calculate</button>
       </main>
     </div>
   );
