ab5c0d2a57d51738fcdb26862b2470195b8828ee
lilao8 <liamgmai1@gmail.com>
2021-08-31 23:49:55 -0400

    change structure and styles

--- src/App.js
+++ src/App.js
@@ -1,57 +1,113 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import firebase from "./firebase";
 import Input from "./Input";
 import Stocks from "./Stocks";
 import Calculation from "./Calculation";
+import axios from "axios";
 
 
 function App() {
-  const [stockList, setStockList] = useState([]);
-
-  useEffect(() => {
-    const dbRef = firebase.database().ref();
-    dbRef.on("value", (response) => {
-      const myData = response.val();
-      const newArray = [];
-      for (const stocks in myData) {
-        const stock = {
-          key: stocks,
-          symbol: myData[stocks].symbol,
-          price: myData[stocks].price,
-          share: myData[stocks].share,
-        };
-        newArray.push(stock);
-      }
-      setStockList(newArray);
-    });
-  }, []);
+    const [stockList, setStockList] = useState([]);
+    const [newList, setNewList] = useState([]);
+    let totalProfit = 0;
 
 
-  return (
-    <div className="App">
-      <header>
-        <h1>Stock Watchlist📈📈📈</h1>
-        <Input stockList={stockList} />
-        <h2>Rule 1: No more than 5 stocks each time.</h2>
-        <h2>Rule 2: Do not try more than 5 times per minute.</h2>
-        <p className="hide">Data from 2021-07-23</p>
-      </header>
 
-      <main>
-        <div className="tables wrapper">
-          <Stocks stockList={stockList} />
-          <Calculation stockList={stockList} />
+    useEffect(() => {
+        const dbRef = firebase.database().ref();
+        dbRef.on("value", (response) => {
+            const myData = response.val();
+            const newArray = [];
+            for (const stocks in myData) {
+                const stock = {
+                    key: stocks,
+                    symbol: myData[stocks].symbol,
+                    price: myData[stocks].price,
+                    share: myData[stocks].share,
+                };
+                newArray.push(stock);
+            }
+            setStockList(newArray);
+        });
+    }, []);
+
+    const handleCalculate = (event) => {
+        event.preventDefault();
+        const newArray = [];
+        const key = "JCWSLD5KCL4HGL0Q";
+        stockList.map((stockObj) => {
+            const promiseObj = axios({
+                url: "https://www.alphavantage.co/query",
+                method: "GET",
+                params: {
+                    function: "TIME_SERIES_DAILY",
+                    symbol: stockObj.symbol,
+                    apikey: key,
+                },
+            });
+            newArray.push(promiseObj);
+            return newArray;
+        });
+
+        Promise.all(newArray)
+            .then((res) => {
+                const profitArray = [];
+
+                res.forEach((obj) => {
+                    stockList.forEach((stockObj) => {
+                        if (stockObj.symbol === obj.data["Meta Data"]["2. Symbol"]) {
+                            const inputPrice = stockObj.price;
+                            const todayPrice =
+                                obj.data["Time Series (Daily)"]["2021-07-23"]["4. close"];
+                            const profitPerStock =
+                                (Math.round(todayPrice - inputPrice) * stockObj.share * 100) /
+                                100;
+                            totalProfit += profitPerStock;
+                            const profit = {profit: profitPerStock, total: totalProfit , key:stockObj.symbol};
+                            const stock1 = {...profit};
+                            profitArray.push(stock1);
+                            console.log(stock1)
+                        }
+                    });
+                });
+                setNewList(profitArray);
+                console.log(profitArray)
+
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    }
+
+    return (
+        <div className="App">
+            <header>
+                <h1>Stock Watchlist📈📈📈</h1>
+                <Input stockList={stockList}/>
+                <h2>Rule 1: No more than 5 stocks each time.</h2>
+                <h2>Rule 2: Do not try more than 5 times per minute.</h2>
+                <p className="hide">Data from 2021-07-23</p>
+                <button className="submit" onClick={handleCalculate}>
+                    Calculate
+                </button>
+            </header>
+
+            <main>
+
+                <div className="tables wrapper">
+                    <Stocks stockList={stockList}/>
+                    <Calculation newList={newList} />
+                </div>
+            </main>
+
+            <footer>
+                Created at
+                <a href="https://junocollege.com/"> Juno College</a>, powered by{" "}
+                <a href="https://www.alphavantage.co/">Alphavantage</a>
+            </footer>
         </div>
-      </main>
-
-      <footer>
-        Created at
-        <a href="https://junocollege.com/"> Juno College</a>, powered by{" "}
-        <a href="https://www.alphavantage.co/">Alphavantage</a>
-      </footer>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/Calculation.js
+++ src/Calculation.js
@@ -1,85 +1,76 @@
-import { useState, useRef } from "react";
-import axios from "axios";
-
 const Calculation = (props) => {
-  const { stockList } = props;
-  const [newList, setNewList] = useState([]);
-  let totalProfit = 0;
-  const results = useRef(null);
+    const {newList} = props;
 
-  const handleCalculate = (e) => {
-    e.preventDefault();
-    const newArray = [];
-    const key = "JCWSLD5KCL4HGL0Q";
 
-    stockList.map((stockObj) => {
-      const promiseObj = axios({
-        url: "https://www.alphavantage.co/query",
-        method: "GET",
-        params: {
-          function: "TIME_SERIES_DAILY",
-          symbol: stockObj.symbol,
-          apikey: key,
-        },
-      });
-      newArray.push(promiseObj);
-      return newArray;
-    });
-    Promise.all(newArray)
-      .then((res) => {
-        const profitArray = [];
-        results.current.scrollIntoView();
-        res.forEach((obj) => {
-          stockList.forEach((stockObj) => {
-            if (stockObj.symbol === obj.data["Meta Data"]["2. Symbol"]) {
-              const inputPrice = stockObj.price;
-              const todayPrice =
-                obj.data["Time Series (Daily)"]["2021-07-23"]["4. close"];
-              const profitPerStock =
-                (Math.round(todayPrice - inputPrice) * stockObj.share * 100) /
-                100;
-              totalProfit += profitPerStock;
-              const profit = { profit: profitPerStock, total: totalProfit };
-              const stock1 = { ...stockObj, ...profit };
-              profitArray.push(stock1);
-            }
-          });
-        });
-        setNewList(profitArray);
-        results.current.scrollIntoView();
-      })
-      .catch((error) => {
-        alert("Please enter correct information or wait for another minute.");
-      });
-  };
-  const positiveValue = (num) =>
-    num > 0 ? "isPositive" : num < 0 ? "isNegative" : "";
+    // handleCalculate = () => {
+    // e.preventDefault();
+    // const newArray = [];
+    // const key = "JCWSLD5KCL4HGL0Q";
+    // stockList.map((stockObj) => {
+    //   const promiseObj = axios({
+    //     url: "https://www.alphavantage.co/query",
+    //     method: "GET",
+    //     params: {
+    //       function: "TIME_SERIES_DAILY",
+    //       symbol: stockObj.symbol,
+    //       apikey: key,
+    //     },
+    //   });
+    //   newArray.push(promiseObj);
+    //   return newArray;
+    // });
+    // Promise.all(newArray)
+    //   .then((res) => {
+    //     const profitArray = [];
+    //     results.current.scrollIntoView();
+    //     res.forEach((obj) => {
+    //       stockList.forEach((stockObj) => {
+    //         if (stockObj.symbol === obj.data["Meta Data"]["2. Symbol"]) {
+    //           const inputPrice = stockObj.price;
+    //           const todayPrice =
+    //             obj.data["Time Series (Daily)"]["2021-07-23"]["4. close"];
+    //           const profitPerStock =
+    //             (Math.round(todayPrice - inputPrice) * stockObj.share * 100) /
+    //             100;
+    //           totalProfit += profitPerStock;
+    //           const profit = { profit: profitPerStock, total: totalProfit };
+    //           const stock1 = { ...stockObj, ...profit };
+    //           profitArray.push(stock1);
+    //         }
+    //       });
+    //     });
+    //     setNewList(profitArray);
+    //     results.current.scrollIntoView();
+    //   })
+    //   .catch((error) => {
+    //     alert("Please enter correct information or wait for another minute.");
+    //   });
+    // };
+    const positiveValue = (num) =>
+        num > 0 ? "isPositive" : num < 0 ? "isNegative" : "";
 
-  return (
-    <div ref={results} className="calculation">
-      <button className="submit" onClick={handleCalculate}>
-        Calculate
-      </button>
-      <div>
-        <div>
-          <ul>
-            <li>Profit</li>
-          </ul>
+    return (
+        <div className="calculation">
+            <div>
+                <div>
+                    <ul>
+                        <li>Profit</li>
+                    </ul>
+                </div>
+                <div>
+                    {newList.map((profitObject) => {
+                        return (
+                            <ul key={profitObject.key}>
+                                <li  className={positiveValue(profitObject.profit)}>
+                                    ${profitObject.profit} USD
+                                </li>
+                            </ul>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
-        <div>
-          {newList.map((profitObject) => {
-            return (
-              <ul key={profitObject.key}>
-                <li className={positiveValue(profitObject.profit)}>
-                  ${profitObject.profit} USD
-                </li>
-              </ul>
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Calculation;
